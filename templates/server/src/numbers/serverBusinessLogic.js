982321cf887565184d480db89357657b912e260b
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2024-09-18 13:18:04 +0200

    Fix linter

--- templates/server/src/numbers/serverBusinessLogic.js
+++ templates/server/src/numbers/serverBusinessLogic.js
@@ -1,6 +1,10 @@
 /**
+ * @typedef {import("@sinch/sdk-core").NumbersCallback} NumbersCallback
+ */
+
+/**
  * Handles a Numbers Event.
- * @param {import("@sinch/sdk-core").NumbersCallback} numbersEvent - The incoming Numbers event notification.
+ * @param {NumbersCallback} numbersEvent - The incoming Numbers event notification.
  */
 export const handleNumbersEvent = async (numbersEvent) => {
   console.log(`Handling Numbers event:\n${JSON.stringify(numbersEvent, null, 2)}`);
